c2c3c5149a4edd6999119e3d1900eaaf88334ac9
Swasti Shreya Mishra <32703974+swastishreya@users.noreply.github.com>
2020-06-29 00:59:12 +0530

    Create music_hub_DML.sql

---
+++ sql/music_hub_DML.sql
@@ -0,0 +1,13 @@
+insert into user values (1, 'swasti', 'swasti_pass','swasti', FALSE);
+insert into user values (2, 'ananya', 'ananya_pass','ananya', TRUE);
+insert into user values (3, 'lalitha', 'lalitha_pass','lalitha', FALSE);
+
+insert into queue values (1,'counting_stars,humnava_mere',FALSE);
+insert into queue values (2,'',FALSE);
+insert into queue values (3,'sweet_but_psycho',FALSE);
+
+insert into song values (1, 'counting_stars', 2);
+insert into song values (2, 'humnava_mere', NULL);
+insert into song values (3, 'sweet_but_psycho', NULL);
+insert into song values (4, 'aakhri_salaam', NULL);
+insert into song values (5, 'i_dont_care', NULL);
